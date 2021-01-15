3cc3c7923e13d5e529531ac14285b2e521cadc2e
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-15 12:54:47 +0200

    add Ukraine map

--- MapKitAnd100CustomMarkers/ViewController.swift
+++ MapKitAnd100CustomMarkers/ViewController.swift
@@ -9,21 +9,36 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class ViewController: UIViewController,CLLocationManagerDelegate {
+class ViewController: UIViewController,MKMapViewDelegate {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    //MARK: - Actions
+    
     
     //MARK: - Properties
     var  manager = CLLocationManager()
     
+    //MARK: - Functions
+    func loadCoordinateUkraine(){
+        let latitudeCoordinate = CLLocationDegrees(48.22)
+        let longitudeCoordinate = CLLocationDegrees(31.10)
+        let locationCoordinate2D = CLLocationCoordinate2D(latitude: latitudeCoordinate, longitude: longitudeCoordinate)
+        let longitudeUkraine = CLLocationDistance(131_600_0)
+        let latitudeUkraine = CLLocationDistance(893_000)
+        
+        let centerUkraine = MKCoordinateRegion(center: locationCoordinate2D, latitudinalMeters: latitudeUkraine, longitudinalMeters:longitudeUkraine)
+        mapView.setRegion(centerUkraine, animated: true)
+    }
+    
 
-    
-    
-    
-    
-    
+        
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       loadCoordinateUkraine()
     }
 
 
