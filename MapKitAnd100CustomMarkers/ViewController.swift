0ce4b685f6271dabae92bc3f744a886887413f42
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-15 21:35:50 +0200

    добавил 100 рандомных маркеров на карту

--- MapKitAnd100CustomMarkers/ViewController.swift
+++ MapKitAnd100CustomMarkers/ViewController.swift
@@ -7,8 +7,6 @@
 
 import UIKit
 import MapKit
-import CoreLocation
-
 
 class ViewController: UIViewController,MKMapViewDelegate {
     
@@ -20,7 +18,9 @@ class ViewController: UIViewController,MKMapViewDelegate {
     
     
     //MARK: - Properties
-    let annotation = MKPointAnnotation()
+    var pin : MKAnnotation!
+   
+  
     let latitudeCoordinate = CLLocationDegrees(49.22)
     let longitudeCoordinate = CLLocationDegrees(31.10)
    
@@ -37,23 +37,61 @@ class ViewController: UIViewController,MKMapViewDelegate {
         mapView.setRegion(centerUkraine, animated: true)
     }
     
-    func addMarker(){
-        annotation.coordinate = CLLocationCoordinate2D(latitude: latitudeCoordinate, longitude: longitudeCoordinate)
-        annotation.title = "This is Ukraine"
-        mapView.addAnnotation(annotation)
-    }
-    
+//    func addAnnotation(){
+//        annotation.coordinate = CLLocationCoordinate2D(latitude: latitudeCoordinate, longitude: longitudeCoordinate)
+//        annotation.title = "Ukraine"
+//        annotation.subtitle = "my small Ukraine"
+//        mapView.addAnnotation(annotation)
+//    }
+//
    
     
- 
-
+    func randomBetweenNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
+    }
+    
+    func generateAnnoLoc() {
+        var num = 0
+        //First we declare While to repeat adding Annotation
+        while num != 100 {
+            num += 1
+            //Add Annotation
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude:CLLocationDegrees(randomBetweenNumbers(firstNum: 46, secondNum: 52)), longitude: CLLocationDegrees(randomBetweenNumbers(firstNum:23, secondNum: 39)))
+            let randomTitleNumber = Int.random(in: 1...100)
+            annotation.title = "\(randomTitleNumber)"
+            annotation.subtitle = "\(annotation.coordinate.longitude)"
+            mapView.addAnnotation(annotation)
+        }
         
+        
+
+    }
+    
+//
+//    func createPointForKiev(){
+//         pin = MyAnnotation(coordinate: CLLocationCoordinate2D(latitude: latitudeCoordinate, longitude: longitudeCoordinate), title: "KIEV", subtitle: "KIEV - FOREVA")
+//        mapView.addAnnotation(pin)
+//    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let annotationView = MKAnnotationView(annotation: pin, reuseIdentifier: "pin")
+        annotationView.image = UIImage(named: "circle")
+        let transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        annotationView.transform = transform
+        return annotationView
+    }
+    
+  
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-       loadCoordinateUkraine()
-        addMarker()
-       
+        mapView.delegate = self
+       // createPointForKiev()
+        
+        loadCoordinateUkraine()
+   //     addAnnotation()
+        generateAnnoLoc()
     }
 
 
