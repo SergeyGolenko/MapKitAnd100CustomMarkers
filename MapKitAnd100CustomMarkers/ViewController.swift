9323b47559aa5cd86bd8d95d03e8fb3bbc0c8a4e
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-15 11:18:19 +0200

    Initial Commit

---
+++ MapKitAnd100CustomMarkers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapKitAnd100CustomMarkers
+//
+//  Created by Сергей Голенко on 15.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
